98deb276a9d194a7c672ed76cafefe6c35e950c1
Alan Hauser <diefthyntis@gmail.com>
2024-08-16 16:10:37 +0200

    Création de la route localhost:3001/api/auth/me par Alan

--- backend-spring/src/main/java/com/diefthyntis/chatop/diefthyntis/controller/AuthentificationController.java
+++ backend-spring/src/main/java/com/diefthyntis/chatop/diefthyntis/controller/AuthentificationController.java
@@ -1,11 +1,13 @@
 package com.diefthyntis.chatop.diefthyntis.controller;
 
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -88,8 +90,12 @@ public class AuthentificationController {
 		 */
 		
 	}
-	
-	
+
+
+	@GetMapping("/me")
+    public ResponseEntity<String> getMe() {
+        return new ResponseEntity<>("Unauthorized", HttpStatus.UNAUTHORIZED);
+    }
 	
 	
 	
